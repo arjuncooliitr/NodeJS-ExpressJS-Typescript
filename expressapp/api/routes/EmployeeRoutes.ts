ec7f8c77a7cae5c314a7c8cd9dc5274af67232b5
Banuprakash <banuprakashc@yahoo.co.in>
2022-05-16 12:05:37 +0530

    day 6

--- expressapp/api/routes/EmployeeRoutes.ts
+++ expressapp/api/routes/EmployeeRoutes.ts
@@ -5,7 +5,7 @@ import EmployeeController from '../controllers/EmployeeController';
 export class EmployeeRoutes {
     constructor(private app:express.Application) {}
     configureRoutes(): express.Application {
-        this.app.routes("/employees")
+        this.app.route("/employees")
         .get(EmployeeController.getEmployees);
         return this.app;
     }
